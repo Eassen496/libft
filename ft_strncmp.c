5b949d7831b965e8b1fe66343bac48279add1e08
Alexis Le Roux <ale-roux@k1r4p13.42mulhouse.fr>
2022-11-08 22:23:08 +0100

    gjkrgkjdbgkdrg

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ale-roux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:56:15 by ale-roux          #+#    #+#             */
-/*   Updated: 2022/11/08 14:17:18 by ale-roux         ###   ########.fr       */
+/*   Updated: 2022/11/08 22:22:18 by ale-roux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i] && n > 0)
+	while (s1[i] && s1[i] == s2[i] && n > 0 && s2[i])
 	{
 		i++;
 		n--;
 	}
 	if (n == 0)
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
